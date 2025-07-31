01a8a9267048074a64e97eb7a0e738db8ab3d1b0
MarcoDava <marco.a.dava@gmail.com>
2025-07-31 15:42:10 -0400

    added new red gradient for users that are on app and place the gradient on user location. This is a temp fix to the heatmap. made useLocation hook work on background and update every 5 minutes

---
+++ crowdsurfer/hooks/useLocationBackground.ts
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
+
+const LOCATION_TASK_NAME = 'background-location-task';
+
+TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    console.error('Background location error:', error);
+    return;
+  }
+  if (data) {
+    const { locations } = data as any;
+    // You can save locations to state, send to server, etc.
+    console.log('Received new locations in background:', locations);
+  }
+});
+
+const useLocationBackground = () => {
+  const [errorMsg, setErrorMsg] = useState<string>('');
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      let { status: bgStatus } = await Location.requestBackgroundPermissionsAsync();
+      if (bgStatus !== 'granted') {
+        setErrorMsg('Permission to access background location was denied');
+        return;
+      }
+      await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+        accuracy: Location.Accuracy.High,
+        timeInterval: 300000, // 5 minutes in ms
+        distanceInterval: 0,
+        showsBackgroundLocationIndicator: true,
+        foregroundService: {
+          notificationTitle: 'CrowdSurfer',
+          notificationBody: 'Tracking your location in the background',
+        },
+      });
+    })();
+
+    return () => {
+      Location.hasStartedLocationUpdatesAsync(LOCATION_TASK_NAME).then((started) => {
+        if (started) Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+      });
+    };
+  }, []);
+
+  // Optionally, get the last known location for display
+  useEffect(() => {
+    (async () => {
+      const last = await Location.getLastKnownPositionAsync();
+      if (last) setLocation(last);
+    })();
+  }, []);
+
+  return { location, errorMsg };
+};
+
+export default useLocationBackground;
